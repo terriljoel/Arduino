297d912c9ef4f70795bf4eb9d06deaaba4d286b9
Terril Joel Nazareth <terriljoel@gmail.com>
2025-02-14 11:57:34 +0100

    test code for printing port change values and control hub

--- HubInformation.ino
+++ HubInformation.ino
@@ -6,51 +6,44 @@ bool isFwVersionAvailable = false;
 bool isBatteryTypeAvailable = false;
 
 bool isInitialized = false;
-void hubPropertyChangeCallback(void *hub, HubPropertyReference hubProperty, uint8_t *pData)
-{
+void hubPropertyChangeCallback(void *hub, HubPropertyReference hubProperty, uint8_t *pData) {
   Lpf2Hub *myHub = (Lpf2Hub *)hub;
 
   Serial.print("HubProperty: ");
   Serial.println((byte)hubProperty, HEX);
 
-  if (hubProperty == HubPropertyReference::RSSI)
-  {
+  if (hubProperty == HubPropertyReference::RSSI) {
     Serial.print("RSSI: ");
     Serial.println(myHub->parseRssi(pData), DEC);
     return;
   }
 
-  if (hubProperty == HubPropertyReference::ADVERTISING_NAME)
-  {
+  if (hubProperty == HubPropertyReference::ADVERTISING_NAME) {
     Serial.print("Advertising Name: ");
     Serial.println(myHub->parseHubAdvertisingName(pData).c_str());
     return;
   }
 
-  if (hubProperty == HubPropertyReference::BATTERY_VOLTAGE)
-  {
+  if (hubProperty == HubPropertyReference::BATTERY_VOLTAGE) {
     Serial.print("BatteryLevel: ");
     Serial.println(myHub->parseBatteryLevel(pData), DEC);
     return;
   }
 
-  if (hubProperty == HubPropertyReference::BUTTON)
-  {
+  if (hubProperty == HubPropertyReference::BUTTON) {
     Serial.print("Button: ");
     Serial.println((byte)myHub->parseHubButton(pData), HEX);
     return;
   }
 
-  if (hubProperty == HubPropertyReference::BATTERY_TYPE)
-  {
+  if (hubProperty == HubPropertyReference::BATTERY_TYPE) {
     Serial.print("BatteryType: ");
     Serial.println(myHub->parseBatteryType(pData), HEX);
-    isBatteryTypeAvailable=true;
+    isBatteryTypeAvailable = true;
     return;
   }
 
-  if (hubProperty == HubPropertyReference::FW_VERSION)
-  {
+  if (hubProperty == HubPropertyReference::FW_VERSION) {
     Version version = myHub->parseVersion(pData);
     Serial.print("FWVersion: ");
     Serial.print(version.Major);
@@ -64,8 +57,7 @@ void hubPropertyChangeCallback(void *hub, HubPropertyReference hubProperty, uint
     return;
   }
 
-  if (hubProperty == HubPropertyReference::HW_VERSION)
-  {
+  if (hubProperty == HubPropertyReference::HW_VERSION) {
     Version version = myHub->parseVersion(pData);
     Serial.print("HWVersion: ");
     Serial.print(version.Major);
@@ -80,28 +72,32 @@ void hubPropertyChangeCallback(void *hub, HubPropertyReference hubProperty, uint
   }
 }
 
-void portValueChangeCallback(void *hub, byte portNumber, DeviceType deviceType, uint8_t *pData)
-{
+void portValueChangeCallback(void *hub, byte portNumber, DeviceType deviceType, uint8_t *pData) {
   Lpf2Hub *myHub = (Lpf2Hub *)hub;
 
-  if (deviceType == DeviceType::VOLTAGE_SENSOR)
-  {
+  if (deviceType == DeviceType::VOLTAGE_SENSOR) {
     double voltage = myHub->parseVoltageSensor(pData);
     Serial.print("Voltage: ");
     Serial.println(voltage, 2);
     return;
   }
 
-  if (deviceType == DeviceType::CURRENT_SENSOR)
-  {
-    double current = myHub->parseCurrentSensor(pData);
-    Serial.print("Current: ");
-    Serial.println(current, 2);
+  if (deviceType == DeviceType::UNKNOWNDEVICE) {
+    double current = myHub->parsePortMessage(pData);
+    Serial.print("Test read: ");
+    // Serial.println(pData);
+    for (size_t i = 0; i < 5; i++) {
+      Serial.println(pData[i]);
+    }
     return;
+  } else {
+    Serial.print("Test read: ");
+    for (size_t i = 0; i < 5; i++) {
+      Serial.println(pData[i]);
+    }
   }
 
-  if (deviceType == DeviceType::MOVE_HUB_TILT_SENSOR)
-  {
+  if (deviceType == DeviceType::MOVE_HUB_TILT_SENSOR) {
     int x = myHub->parseBoostTiltSensorX(pData);
     int y = myHub->parseBoostTiltSensorY(pData);
     Serial.print("Tilt X: ");
@@ -138,30 +134,27 @@ void loop() {
       myTrainHub.activateHubPropertyUpdate(HubPropertyReference::BUTTON, hubPropertyChangeCallback);
       delay(50);
       myTrainHub.activateHubPropertyUpdate(HubPropertyReference::RSSI, hubPropertyChangeCallback);
-      myTrainHub.activatePortDevice((byte)MoveHubPort::TILT, portValueChangeCallback);
       delay(50);
-      myTrainHub.activatePortDevice((byte)MoveHubPort::CURRENT, portValueChangeCallback);
+      myTrainHub.activatePortDevice((byte)PoweredUpHubPort::A, portValueChangeCallback);
       delay(50);
-      myTrainHub.activatePortDevice((byte)MoveHubPort::VOLTAGE, portValueChangeCallback);
+      // myTrainHub.activatePortDevice((byte)MoveHubPort::CURRENT, portValueChangeCallback);
+      // delay(50);s
+      // myTrainHub.activatePortDevice((byte)MoveHubPort::VOLTAGE, portValueChangeCallback);
       isInitialized = true;
     } else {
       Serial.println("Failed to connect to HUB");
     }
   }
-  if (myTrainHub.isConnected() && !isFwVersionAvailable)
-  {
+  if (myTrainHub.isConnected() && !isFwVersionAvailable) {
     myTrainHub.requestHubPropertyUpdate(HubPropertyReference::FW_VERSION, hubPropertyChangeCallback);
     delay(100);
   }
-  if (myTrainHub.isConnected() && !isHwVersionAvailable)
-  {
+  if (myTrainHub.isConnected() && !isHwVersionAvailable) {
     myTrainHub.requestHubPropertyUpdate(HubPropertyReference::HW_VERSION, hubPropertyChangeCallback);
     delay(100);
   }
-  if (myTrainHub.isConnected() && !isBatteryTypeAvailable)
-  {
+  if (myTrainHub.isConnected() && !isBatteryTypeAvailable) {
     myTrainHub.requestHubPropertyUpdate(HubPropertyReference::BATTERY_TYPE, hubPropertyChangeCallback);
     delay(100);
   }
-
 }
